6aaea56b872fc49ee5e80b164a8c67db0d439aa9
LauraNavRuso <laura.nav.ruso@gmail.com>
2018-03-09 22:34:52 +0100

    create basic component structure

---
+++ src/PokemonCard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class PokemonCard extends Component {
+  render() {
+    return (
+      <div className="App">
+        Soy un pokemon en concreto
+      </div>
+    );
+  }
+}
+
+export default PokemonCard;
